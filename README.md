52c3dfe7a8e1faa77f693dc9ff774c5a868a2437
Alexandru Constantin <alex@devmotion.ro>
2020-08-07 19:02:52 +0300

    strapi generate plugin boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# Strapi plugin strapi:faker
+
+A quick description of strapi:faker.
---
+++ controllers/strapi-faker.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * strapi-faker.js controller
+ *
+ * @description: A set of functions called "actions" of the `strapi-faker` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok'
+    });
+  }
+};
